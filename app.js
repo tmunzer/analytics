58365a3b5cd106d7b7b154f80b6044ce32f33951
tmunzer <tmunzer@aerohive.com>
2016-02-16 22:13:28 +0100

    Revert "000"
    This reverts commit c823d532893181bf1af08352964030d788040f7e.

--- app.js
+++ app.js
@@ -1,87 +1,44 @@
-'use strict';
-//===============CREATE ROOT PATH=================
+
 var path = require('path');
 global.appRoot = path.resolve(__dirname);
 
-//===============DEPENDENCIES=================
-var http = require('http');
-var https = require('https');
-var LEX = require('letsencrypt-express').testing();
-var LE = require('letsencrypt');
 var express = require('express');
 var parseurl = require('parseurl');
 var session = require('express-session');
 var favicon = require('serve-favicon');
+var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-//===============ROUTES DEPENDENCIES=================
 var routes = require('./routes/init');
 var dashboard = require('./routes/dashboard');
 var details = require('./routes/details');
 var api = require('./routes/api');
 
-//===============EVENT EMITTER=================
 var events = require('events');
 global.eventEmitter = new events.EventEmitter();
 
-//===============CREATE APP=================
+
 var app = express();
 
-//=============CREATE LOGGER===============
-var winston = require('winston');
-winston.emitErrs = true;
-var logger = new winston.Logger({
-    transports: [
-        new winston.transports.File({
-            level: 'info',
-            filename: __dirname + '/logs/all-logs.log',
-            handleExceptions: true,
-            json: true,
-            maxsize: 5242880, //5MB
-            maxFiles: 5,
-            colorize: false
-        }),
-        new winston.transports.Console({
-            level: 'debug',
-            handleExceptions: true,
-            json: false,
-            colorize: true
-        })
-    ],
-    exitOnError: false
-});
-
-module.exports.logger = logger;
-module.exports.logger.stream = {
-    write: function(message, encoding){
-        logger.info(message);
-    }
-};
-
-logger.debug("Overriding 'Express' logger");
-app.use(require('morgan')("default", { "stream": logger.stream }));
-
-//===============SESSION=================
 app.use(session({
     secret: 'Aerohive Analytics Ref APP',
     resave: false,
     saveUninitialized: true
 }));
 
-//===============CONF APP=================
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'static')));
 
-//===============ROUTES=================
 app.use('/', routes);
 app.use('/dashboard/', dashboard);
 app.use('/details/', details);
@@ -94,8 +51,6 @@ app.get('/fail', function (req, res, next) {
     res.send('Hello World');
   }, 1000);
 });
-
-//===============ERRORS=================
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -129,62 +84,3 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
-
-var lex = LEX.create({
-
-     lifetime: 90 * 24 * 60 * 60 * 1000    // expect certificates to last 90 days
-    , failedWait: 5 * 60 * 1000             // if registering fails wait 5 minutes before trying again
-    , renewWithin: 3 * 24 * 60 * 60 * 1000  // renew at least 3 days before expiration
-    , memorizeFor: 1 * 24 * 60 * 60 * 1000  // keep certificates in memory for 1 day
-
-    , approveRegistration: function (hostname, cb) {
-        cb(null, {
-            domains: [hostname]
-            , email: 'tmunzer@aerohive.com'
-            , agreeTos: true
-        });
-    }
-
-    , handleRenewFailure: function (err, hostname, certInfo) {
-        console.error("ERROR: Failed to renew domain '", hostname, "':");
-        if (err) {
-            console.error(err.stack || err);
-        }
-        if (certInfo) {
-            console.error(certInfo);
-        }
-    }
-
-    , letsencrypt: LE.create(
-        // options
-        { configDir: './letsencrypt.config'
-            , manual: true
-
-            , server: LE.productionServerUrl
-            , privkeyPath: LE.privkeyPath
-            , fullchainPath: LE.fullchainPath
-            , certPath: LE.certPath
-            , chainPath: LE.chainPath
-            , renewalPath: LE.renewalPath
-            , accountsDir: LE.accountsDir
-
-            , debug: true
-        }
-
-        // handlers
-        , { setChallenge: LEX.setChallenge
-            , removeChallenge: LEX.removeChallenge
-        }
-    )
-
-    , debug: false
-});
-
-http.createServer(LEX.createAcmeResponder(lex, function (req, res) {
-    res.setHeader('Location', 'https://' + req.headers.host + req.url);
-    res.end('<!-- Hello Mr Developer! Please use HTTPS instead -->');
-}));
-
-https.createServer(lex.httpsOptions, LEX.createAcmeResponder(lex, function (req, res) {
-    res.end('Hello!');
-}));
--- node_modules/letsencrypt-express/node_modules/letsencrypt/node_modules/letiny-core/lib/letsencrypt-node-crypto.js
+++
@@ -1,8 +0,0 @@
-/*!
- * letiny-core
- * Copyright(c) 2015 AJ ONeal <aj@daplie.com> https://daplie.com
- * Apache-2.0 OR MIT (and hence also MPL 2.0)
-*/
-'use strict';
-
-module.exports = {};
