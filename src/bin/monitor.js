96b650cc39af4531b9b9e719eaeac6a517dd4db5
Thomas Munzer <tmunzer@aerohive.com>
2017-03-12 11:09:40 +0100

    small bug fixes

--- src/bin/monitor.js
+++ src/bin/monitor.js
@@ -2,13 +2,13 @@
 var respawn = require('respawn');
 
 var monitor = respawn(['node', 'www'], {
-    env: {ENV_VAR:'test'}, // set env vars
+    env: { ENV_VAR: 'test' }, // set env vars
     cwd: '.',              // set cwd
-    maxRestarts:100,        // how many restarts are allowed within 60s
-    sleep:1000            // time to sleep between restarts
+    maxRestarts: 100,        // how many restarts are allowed within 60s
+    sleep: 1000            // time to sleep between restarts
 });
 monitor.on('spawn', function () {
-    console.info("\x1b[32minfo\x1b[0m:",'application monitor started...');
+    console.info("\x1b[32minfo\x1b[0m:", 'application monitor started...');
 });
 
 monitor.on('exit', function (code, signal) {
@@ -16,11 +16,11 @@ monitor.on('exit', function (code, signal) {
 });
 
 monitor.on('stdout', function (data) {
-    console.info("\x1b[32minfo\x1b[0m:",data.toString());
+    console.info("\x1b[32minfo\x1b[0m:", data.toString());
 });
 
 monitor.on('stderr', function (data) {
-    console.error("\x1b[31mERROR\x1b[0m:", 'process error: '+ data.toString());
+    console.error("\x1b[31mERROR\x1b[0m:", 'process error: ' + data.toString());
 });
 
 monitor.start(); // spawn and watch
