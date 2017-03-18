62d685fb62dc1583044e8cb30acf21f548de8e86
Thomas Munzer <tmunzer@aerohive.com>
2017-03-18 14:57:11 +0100

    improve error rendering

--- src/routes/error.js
+++ src/routes/error.js
@@ -1,9 +1,18 @@
-module.exports.render = function(error, currentPage, req, res) {
+var app = require("../app");
+
+module.exports.render = function (error, currentPage, req, res) {
+    var status, message, stack;
     console.error("\x1b[31mERROR\x1b[0m:", error);
-    res.render("error", {
+    if (error.status) status = error.status;
+    else status = 500;
+    if (error.message) message = error.message;
+    else message = error;
+    if (error.stack && app.get('env') === 'development') stack = error.stack;
+    else stack = "";
+    res.status(status).render("error", {
         current_page: currentPage,
-        error: error,
-        user: req.user,
-        session: req.session
+        status: status,
+        message: message,
+        stack: stack
     });
 };
